cda4e2cb3cc95b76a22145be09ca3fe8575cf7f4
Habeba-Elhosary <habebaelhosary98@gmail.com>
2024-02-05 23:24:58 +0200

    -Added flutter_svg & flutter_screenutil -Made a file for doc app client -Added navigation extension -Added routing folder that contains router class and routes files -Added color & text styles -Added empty login screen -Added onboarding screen

---
+++ lib/features/onboarding/widgets/get_started_button.dart
@@ -0,0 +1,29 @@
+import 'package:doctor_appointment_app/core/helpers/extensions.dart';
+import 'package:doctor_appointment_app/core/routing/routes.dart';
+import 'package:doctor_appointment_app/core/theming/colors.dart';
+import 'package:doctor_appointment_app/core/theming/styles.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class GetStartedButton extends StatelessWidget {
+  const GetStartedButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {
+        context.pushNamed(Routes.loginScreen);
+      },
+      style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(ColorsManager.blue),
+          tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+          minimumSize: MaterialStateProperty.all(
+            const Size(double.infinity, 52),
+          ),
+          shape: MaterialStateProperty.all(RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16.r),
+          ))),
+      child: Text('Get Started', style: TextStyles.font16WhiteSemiBold),
+    );
+  }
+}
