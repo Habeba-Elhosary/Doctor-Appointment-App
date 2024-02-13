86626787c5d3f25a7c4d2b5aa9bbaca584114ff3
Habeba-Elhosary <habebaelhosary98@gmail.com>
2024-02-13 23:48:49 +0200

    -Added Development and Production flavors for android -Changed android app name for each flavor -Added launch.json for flavors config -Added two main files for each flavor

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,10 +1,12 @@
 import 'package:doctor_appointment_app/core/di/dependency_injection.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'core/routing/app_router.dart';
 import 'doc_app.dart';
 
-void main() {
+void main() async {
   setUpGetIt();
+  await ScreenUtil.ensureScreenSize();
   runApp(
     DocApp(
       appRouter: AppRouter(),
